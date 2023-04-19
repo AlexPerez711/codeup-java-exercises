4780bb1d09ce0728d53b4deaa652d0408f10b905
Alex Perez <alex.f.perez92@gmail.com>
2023-04-19 10:01:15 -0500

    finished string / Bob exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String q1 = "We don't need no education";
+        System.out.println(q1.replace("education", "thought control"));
+
+        String q2 = "\nCheck \"this\" out!, \"s inside of \"s!";
+        System.out.println(q2);
+
+        String q3 = "\nIn windows, the main drive is usually C:\\";
+        System.out.println(q3);
+
+        String q4 = "\nI can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(q4);
+    }
+}
