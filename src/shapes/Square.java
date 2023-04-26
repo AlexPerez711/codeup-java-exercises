34c1117333ded35e41d5135a70a182b18309b1ae
Alex Perez <alex.f.perez92@gmail.com>
2023-04-26 11:08:19 -0500

    Interfaces and Abstract Exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,46 @@
 package shapes;
+//
+//public class Square extends Rectangle {
+//    public double side;
+//
+//    public Square(double side){
+//        super(side, side);
+//        this.side = side;
+//    }
+//
+//    public double getPerimeter(){
+//        return side * 4;
+//    }
+//
+//    public double getArea(){
+//        return Math.pow(side, 2);
+//    }
+//}
 
-public class Square extends Rectangle {
-    public double side;
-
-    public Square(double side){
+public class Square extends Quadrilateral{
+    public Square(double side) {
         super(side, side);
-        this.side = side;
     }
 
-    public double getPerimeter(){
-        return side * 4;
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
     }
 
-    public double getArea(){
-        return Math.pow(side, 2);
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
+        this.length = width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
     }
 }
